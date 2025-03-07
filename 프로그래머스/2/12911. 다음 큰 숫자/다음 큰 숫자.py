fbce46752fc08dffd4e555f9d4332b0f4665b598
GyeongHun <54014203+mental-disaster@users.noreply.github.com>
2025-03-07 09:51:14 +0900

    Update 다음 큰 숫자.py
    불필요한 주석 제거

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,3 +1,2 @@
 def solution(n):
-    # return int(bin(0b1 << (list(reversed(bin(n))).index('1') if(int(list(reversed(bin(n))).index('1'))) else 1)) + bin(n >> list(reversed(bin(n))).index('1')+1)[2:], 2)
     return n + 1 + ((n & ((1 << n.bit_length()) - 1)) >> 1) if n & 1 else 0
